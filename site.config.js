9d05ea370c5bb54df9292b1eba394f6a3fda1043
YeojinChoi <yesjin1209@gmail.com>
2024-07-10 12:56:10 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -28,9 +28,6 @@ const CONFIG = {
   since: 2024, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
-  seo: {
-    keyworkds: ["Blog","Website","Notion"]
-  },
   // notion configuration (required)
   notionConfig: {
     pageId: process.env.NOTION_PAGE_ID,
@@ -73,5 +70,7 @@ const CONFIG = {
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
+
+console.log(process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID);
 
 module.exports = { CONFIG }
